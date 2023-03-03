4747689105a4ba941a5b230cd5c93f878610d18c
joceilton <joceilton.g@gmail.com>
2023-03-03 14:05:08 -0300

    Create adsterrabanner18371541.js

---
+++ adsterrabanner18371541.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'c061e55446fa4cdbcd5eb713a7656f2f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c061e55446fa4cdbcd5eb713a7656f2f/invoke.js"></scr' + 'ipt>');
+</script>
